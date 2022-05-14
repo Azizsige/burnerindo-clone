57e6ec5ed8261a9509032d15d864d3d14021278f
Azizsige <kurniawan00azizsige@gmail.com>
2022-05-14 16:38:37 +0700

    update: add section 4 but not responsive

--- dist/js/app.js
+++ dist/js/app.js
@@ -46,3 +46,38 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
+var swiper = new Swiper(".swiper-sale", {
+  slidesPerView: 5,
+        spaceBetween: 30,
+        loop: "true",
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+});
+var swiper = new Swiper(".swiper-Bestsale", {
+  slidesPerView: 5,
+        spaceBetween: 30,
+        loop: "true",
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+});
+
