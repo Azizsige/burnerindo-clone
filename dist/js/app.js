3d9facd38008973e7ee3b851a8a8845cf66d6ce9
azizsige <kurniawan00azizsige@gmail.com>
2022-05-24 13:36:45 +0700

    update: modified

--- dist/js/app.js
+++ dist/js/app.js
@@ -102,7 +102,7 @@ $(".owl-flash").owlCarousel({
   margin: 10,
   nav: true,
   lazyLoad: true,
-  autoplay: true,
+  // autoplay: true,
   autoplayTimeout: 2000,
   autoplayHoverPause: true,
   responsive: {
@@ -125,7 +125,7 @@ $(".owl-flash").owlCarousel({
       loop: true,
     },
     1536: {
-      items: 2,
+      items: 3,
       nav: true,
       loop: true,
     },
@@ -136,7 +136,7 @@ $(".owl-best").owlCarousel({
   margin: 10,
   nav: true,
   lazyLoad: true,
-  autoplay: true,
+  // autoplay: true,
   autoplayTimeout: 2000,
   autoplayHoverPause: true,
   responsive: {
