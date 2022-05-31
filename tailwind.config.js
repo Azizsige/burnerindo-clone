e67d6fabf51f7ecae8ded2fd952f45766123e123
azizsige <kurniawan00azizsige@gmail.com>
2022-05-31 13:38:31 +0700

    style: search bar in desktop version

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
     extend: {
       colors: {
         primary: "#cc0000",
-        kategoriHero: 'rgba(232, 227, 227, .5)'
+        kategoriHero: "rgba(232, 227, 227, .5)",
+        ready:
+          "linear-gradient(90deg, rgb(0 255 67) 0%, rgb(9 121 23) 75%, rgb(17 111 33) 100%)",
+        sold: "#696969",
       },
     },
   },
